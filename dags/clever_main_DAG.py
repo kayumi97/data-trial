f62939a242eb482fdb38b29f4a4d609774275b28
Karina Sakurai <karina.sakurai@usp.br>
2024-10-15 01:42:51 -0300

    Forking the repository with the updates and solutions requested for the Data Engineer trial assignment, including data processing, ranking, and sentiment analysis.

--- dags/clever_main_DAG.py
+++ dags/clever_main_DAG.py
@@ -2,6 +2,8 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator
+from scripts.clever_main_pipeline import upload_to_postgres
+
 
 default_args = {
     "owner": "alec.ventura",
@@ -31,7 +33,8 @@ with DAG("clever_main_DAG", default_args=default_args, catchup=False, schedule_i
             task_id=task_id,
             python_callable=upload_to_postgres,
             dag=dag,
-            execution_timeout=timedelta(milliseconds=2),
+            #execution_timeout=timedelta(milliseconds=2),
+            execution_timeout=timedelta(minutes=10),
             op_kwargs={
                 "file_name": file
             }
--- dags/scripts/clever_main_pipeline.py
+++ dags/scripts/clever_main_pipeline.py
@@ -1,13 +1,27 @@
 import pandas as pd
 from scripts.postgres_helper import upload_overwrite_table
 
+def process_and_clean_csv(file_path, output_file):
+    try:
+        df = pd.read_csv(file_path, sep=',', quotechar='"', escapechar='\\', encoding='latin1', on_bad_lines='skip', skip_blank_lines=True)
+
+        print("Visualização do arquivo original corrigido:")
+        print(df.head())
+
+        df.to_csv(output_file, sep=',', index=False)
+        print(f"Arquivo corrigido salvo como {output_file}")
+
+    except Exception as e:
+        print(f"Erro ao processar o arquivo CSV: {e}")
 
 def upload_to_postgres(**kwargs):
-    file_name=kwargs.get('file_name')
+    file_name = kwargs.get('file_name')
     table_name = file_name.split('.')[0]
-    
-    raw_df = pd.read_csv(
-        f'dags/scripts/data_examples/{file_name}'
-    )
+    input_file = f'dags/scripts/data_examples/{file_name}'
+    output_file = f'dags/scripts/data_examples/{table_name}_cleaned.csv'
+
+    process_and_clean_csv(input_file, output_file)
+
+    raw_df = pd.read_csv(output_file)
 
     upload_overwrite_table(raw_df, table_name)
