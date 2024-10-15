d4399891b0d0c54277a544cd8a8365b262d77869
Karina Sakurai <karina.sakurai@usp.br>
2024-10-15 01:46:36 -0300

    Removing comments in portuguese.

--- dags/scripts/clever_main_pipeline.py
+++ dags/scripts/clever_main_pipeline.py
@@ -4,15 +4,11 @@ from scripts.postgres_helper import upload_overwrite_table
 def process_and_clean_csv(file_path, output_file):
     try:
         df = pd.read_csv(file_path, sep=',', quotechar='"', escapechar='\\', encoding='latin1', on_bad_lines='skip', skip_blank_lines=True)
-
-        print("Visualização do arquivo original corrigido:")
         print(df.head())
-
         df.to_csv(output_file, sep=',', index=False)
-        print(f"Arquivo corrigido salvo como {output_file}")
 
     except Exception as e:
-        print(f"Erro ao processar o arquivo CSV: {e}")
+        print(f"Error: {e}")
 
 def upload_to_postgres(**kwargs):
     file_name = kwargs.get('file_name')
